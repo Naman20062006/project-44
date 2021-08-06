88f2ab89012aaf5c308b87097eba02aff1508e93
Naman20062006 <79857495+Naman20062006@users.noreply.github.com>
2021-08-06 20:17:31 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,22 @@
+class System{
+
+    constructor(){}
+
+    authenticater(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+
+         if(actualCode===enteredCode.toUpperCase()){
+             return true
+         }else{
+             return false
+         }
+
+
+
+    }
+
+    
+
+}
